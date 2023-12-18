1e292ea7f376639dfd6d9ea792611f24ff127982
= <=>
2023-12-18 11:53:01 +0300

    modified:   app/Controllers/Http/DoorsController.ts 	modified:   resources/views/emails/doorstatus.edge

--- app/Controllers/Http/DoorsController.ts
+++ app/Controllers/Http/DoorsController.ts
@@ -127,40 +127,72 @@ export default class DoorsController {
   //   }
   // }
   
-  public async update({request, response}: HttpContextContract){
+  // public async update({request, response}: HttpContextContract){
 
-    try {
+  //   try {
   
-      const { id } =  request.params();
-      const data = request.only(['mgr_doors_name', 'mgr_doors_state', 'mgr_doors_ip', 'mgr_doors_location', 'mgr_doors_terminal']);
-        const door = await Door.findOrFail(id);
-        const door_state =  data.mgr_doors_state;
-        door.merge(data);
-        await door.save();
+  //     const { id } =  request.params();
+  //     const data = request.only(['mgr_doors_name', 'mgr_doors_state', 'mgr_doors_ip', 'mgr_doors_location', 'mgr_doors_terminal']);
+  //       const door = await Door.findOrFail(id);
+  //       const door_state =  data.mgr_doors_state;
+  //       const time = Date.now();
+  //       door.merge(data);
+  //       await door.save();
   
   
-        const last_log = await Accesslog.query().orderBy('id', 'desc').first();
-        console.log("last log",last_log);
-        const last_state = last_log?.mgr_accesslogs_state;
-        console.log("last state",last_state);
+  //       const last_log = await Accesslog.query().orderBy('id', 'desc').first();
+
+  //       //compare last_log.mgr_accesslogs_door with door.id if same then compare time
+  //       const last_door = last_log.gbh_mgrmdraccesslogs_doors;
+
+
+  //       //check if last log is for the same door, if yes compare time
+
+  //       if (last_door == door.id){
+  //         const last_timelog = new Date(last_log.mgr_accesslogs_time).getTime();
+  //         console.log("last time log",last_timelog);
+  //         const time_diff = time - last_timelog;
+  //         console.log("time diff",time_diff);
+  //         //send mail if time difference is greater than 2 minutes
+  //         if (time_diff > 120000){
+  //           const doordata = await Door.query().where('id', id).preload('terminal').preload('location').firstOrFail()
+  //           //send email simple without html view
+  //           if (door.mgr_doors_state == '1') {
+  //             await Mail.send((message) => {
+  //                         message
+  //                           .from('ictadmin.noreply@grainbulk.com')
+  //                           .to('michael.mwasela@grainbulk.com')
+  //                           .subject('Door Status Infringement')
+  //                           //terminal and location are foreign keys in the door model
+  //                           .htmlView('emails/doorstatus', { door: doordata })
+
+  //               }
+  //               )
+  //             }
+  //           }
+  //       }   
+
+  //       //console.log("last log",last_log);
+  //       const last_state = last_log?.mgr_accesslogs_state;
+  //       //console.log("last state",last_state);
   
-        if (door_state !== last_state){
-          await Accesslog.create({
+  //       if (door_state !== last_state){
+  //         await Accesslog.create({
   
-            mgr_accesslogs_state: door.mgr_doors_state,
-              gbh_mgrmdraccesslogs_doors: door.id,
-              gbh_mgrmdraccesslogs_locations: door.mgr_doors_location,
-              gbh_mgrmdraccesslogs_terminal: door.mgr_doors_terminal
+  //             mgr_accesslogs_state: door.mgr_doors_state,
+  //             gbh_mgrmdraccesslogs_doors: door.id,
+  //             gbh_mgrmdraccesslogs_locations: door.mgr_doors_location,
+  //             gbh_mgrmdraccesslogs_terminal: door.mgr_doors_terminal
   
-          })
-        } else {
-          return response.status(202).json(door);
-    }
-  } catch (error) {
-    console.error(error);
-    return response.status(500).json({ error: 'Internal Server Error' });
-  }
-  }
+  //         })
+  //       } else {
+  //         return response.status(202).json(door);
+  //   }
+  // } catch (error) {
+  //   console.error(error);
+  //   return response.status(500).json({ error: 'Internal Server Error' });
+  // }
+  // }
 
 
   // public async update({ request, response }: HttpContextContract) {
@@ -215,6 +247,56 @@ export default class DoorsController {
   //     return response.status(200).json(door);
   //   }
   // }
+
+  public async update({ request, response }: HttpContextContract) {
+    try {
+      const { id } = request.params();
+      const data = request.only(['mgr_doors_name', 'mgr_doors_state', 'mgr_doors_ip', 'mgr_doors_location', 'mgr_doors_terminal']);
+      const door = await Door.findOrFail(id);
+  
+      const door_state = data.mgr_doors_state;
+      const time = Date.now();
+  
+      door.merge(data);
+      await door.save();
+  
+      const last_log = await Accesslog.query().orderBy('id', 'desc').first();
+      const last_door = last_log.gbh_mgrmdraccesslogs_doors;
+      const last_state = last_log?.mgr_accesslogs_state;
+  
+      if (last_door == door.id && last_state == '1') {
+        const last_timelog = new Date(last_log.mgr_accesslogs_time).getTime();
+        const time_diff = time - last_timelog;
+  
+        if (time_diff > 120000 && door_state == '0') {
+          // Door has been closed after being opened for more than 2 minutes, send email
+          const doordata = await Door.query().where('id', id).preload('terminal').preload('location').firstOrFail();
+          await Mail.send((message) => {
+            message
+              .from('ictadmin.noreply@grainbulk.com')
+              .to('michael.mwasela@grainbulk.com')
+              .subject('Grainbulk Door Alerts')
+              .htmlView('emails/doorstatus', { door: doordata });
+          });
+        }
+      }
+  
+      if (door_state !== last_state) {
+        await Accesslog.create({
+          mgr_accesslogs_state: door.mgr_doors_state,
+          gbh_mgrmdraccesslogs_doors: door.id,
+          gbh_mgrmdraccesslogs_locations: door.mgr_doors_location,
+          gbh_mgrmdraccesslogs_terminal: door.mgr_doors_terminal,
+        });
+      } else {
+        return response.status(202).json(door);
+      }
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+  
 
 
   public async destroy({ request, response }: HttpContextContract) {
